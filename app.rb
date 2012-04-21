34e027c6ca4eef235e71848d164717bd114ab526
Russell Osborne <russell@burningpony.com>
2012-04-21 15:07:56 -0400

    Refactoring and adding passthrough to WKHTMLTOPDF options

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'haml'
 require 'uri'
 require 'anemone'
 require 'resque'
-
+require './worker'
 
 configure :production do
   require 'newrelic_rpm'
@@ -14,7 +14,7 @@ uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 get '/' do
-  haml :index, :format => :html5
+  haml :index, :format => :html5, :layout => :application
 end
 # Handle a Post to the root URL
 # Pass back an array of spidered Links
@@ -22,13 +22,14 @@ post '/' do
   url = params["url"]
   confirm = params["confirm"]
   @processed_urls = []
+  @limit = 9
   Anemone.crawl(url, :depth_limit => 1, :obey_robots => true) do |anemone|
     anemone.on_every_page do |page|
       @processed_urls << page.url.to_s
 
     end
   end
-  haml :index, :format => :html5
+  haml :prepare, :format => :html5, :layout => :application
 end
 
 post '/process' do
@@ -37,10 +38,10 @@ post '/process' do
   confirm = params["confirm"]
   @processed_urls = params["urls"].map {|i, l| l} 
   if email 
-    @job = Resque.enqueue(Pdfs, {:email => email, :url => url, :urls_to_process => @processed_urls})
+    @job = Resque.enqueue(Pdfs, {:email => email, :url => url, :urls_to_process => @processed_urls, :options => {} })
   end
 
-  haml :index, :format => :html5
+  haml :index, :format => :html5, :layout => :application
 
 end
 

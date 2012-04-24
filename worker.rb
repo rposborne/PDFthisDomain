2df9fd4962e382ebbb6156afa6c87580e7627ca6
Russell Osborne <russell@burningpony.com>
2012-04-23 20:26:33 -0400

    typo

--- worker.rb
+++ worker.rb
@@ -91,7 +91,7 @@ def render_url_to_pdf(url, options={})
   t = Tempfile.new("#{Time.now}")
   puts "Rendering Link #{url}  to #{t.path}"
   wk = File.join(ENV['APP_ROOT'], "bin", "wkhtmltopdf-amd64")
-  command = "\"#{wk}\" #{url} #{Shellwords.escape(t.path)} #{parse_options(options.nil? {} : options)}"
+  command = "\"#{wk}\" #{url} #{Shellwords.escape(t.path)} #{parse_options(options.nil? ? {} : options)}"
   puts "Command Passed to wkhtmltopdf #{command}"
   system("#{command}")
   

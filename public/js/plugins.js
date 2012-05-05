58196f9f004ba32a305059813c24edf5c59b6074
Russell Osborne <russosborn@gmail.com>
2012-05-04 21:22:01 -0400

    Tiny Touch ups

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,4 +1,7 @@
-
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
 
 function formatCurrency(num) {
   num = num.toString().replace(/\$|\,/g,'');
@@ -87,11 +90,8 @@ $('.to_modal').click(function(e) {
     $(this).addClass("disabled")
     var href = $(e.target).attr('href');
     mixpanel.track('Previewed URL', { "Url": href,'mp_note': href, });
-    if (href.indexOf('#') == 0) {
-        $(href).modal('open');
-    } else {
-      $('<div class="modal fade" ><img src=' + href + '/></div>').modal();  
-    }
+    $('<div class="modal fade" ><img src=' + href + '/></div>').modal();  
+    
 });
 
 $('.to_prevew_modal').click(function(e) {

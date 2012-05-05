1f9da5eeddd0908b645ecafdb0da4ab9c2508b36
Russell Osborne <russosborn@gmail.com>
2012-05-05 11:45:22 -0400

    Improved Tracking

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,3 +1,4 @@
+mixpanel.track_forms("#prepare-form", "#url-lookup","#confirm-email");
 
 function isValidEmailAddress(emailAddress) {
   var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
@@ -105,7 +106,6 @@ $(".catch").click(function(e) {
 
 
   $('.to_modal').click(function(e) {
-
     e.preventDefault();
     e.stopPropagation();
     $(this).button('loading');
@@ -113,26 +113,22 @@ $(".catch").click(function(e) {
     var href = $(e.target).attr('href');
     mixpanel.track('Previewed URL', { "Url": href,'mp_note': href, });
     $('<div class="modal fade" ><img src=' + href + '/></div>').modal();  
-
+    $(this).replaceWith("");
   });
 
   $('.to_prevew_modal').click(function(e) {
-
     e.preventDefault();
     e.stopPropagation();
     console.log(this)
     var href = $(this).attr('href');
-
     $('<div class="modal fade" ><img src=' + href + '/></div>').modal();
     mixpanel.track('Viewed Example', { 'mp_note': href, });
   });
 
   function store_urls() {
-
     var checked = $("input.urls:checked").map(function() {
       return $(this).val();
     })
-
     store.set('urls_to_process', checked.toArray() );
     mixpanel.track('URLS Stored',{'urls_to_process': checked.toArray()});
   }
